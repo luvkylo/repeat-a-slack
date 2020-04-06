69bab9c0ad0564862a65bb9803b645e2dcad6cf4
Alvin <alvinhewitalvin@gmail.com>
2020-04-06 11:01:53 -0700

    added different env names to avoid clashing from different scripts

--- property.js
+++ property.js
@@ -5,6 +5,9 @@ const property = {
     prefix: process.env.S3_BUCKET_PREFIX,
     jsonPutKeyFolder: process.env.S3_JSON_PUT_KEY_FOLDER,
     csvPutKeyFolder: process.env.S3_CSV_PUT_KEY_FOLDER,
+    tvaugFromBucketName: process.env.S3_TVAUG_FROM_BUCKET_NAME,
+    tvaugPrefix: process.env.S3_TVAUG_BUCKET_PREFIX,
+    tvaugJsonPutKeyFolder: process.env.S3_TVAUG_JSON_PUT_KEY_FOLDER,
   },
   redshift: {
     user: process.env.REDSHIFT_USER,
