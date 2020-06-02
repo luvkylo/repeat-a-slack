3e730f98fb58d4ff487da6cbc67171b3c99b1674
Alvin <alvinhewitalvin@gmail.com>
2020-06-01 20:12:06 -0700

    removed development component

--- multiple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for i in $(seq 1 $1)
-do  
-    echo '******************Working on Day '$i'********************'
-    DAY=$i node mediatailor_quicksight.js
-    echo '******************Done with Day '$i'*********************'
-done
