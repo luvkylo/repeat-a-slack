9c600c707cc1b84f9323f45abefc945de4012b7c
Alvin <alvinhewitalvin@gmail.com>
2021-01-12 09:48:16 -0800

    created a separate class for queries

--- fastly/video_attribution.py
+++ fastly/video_attribution.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from service import env
 
 from db import sql
+from db import queries
 
 
 def main():
@@ -24,9 +25,12 @@ def main():
     newCompleted = time.strftime("%Y-%m-%d %H:00:00", start)
 
     env_var = env.Env()
+    queries = queries.Queries()
 
     print("Connecting to Redshift...")
     # connect to Redshift
+    # On QA, this will be connected to the QA reporting Redshift database
+    # On PROD, this will be connected to the PROD reporting Redshift database
     redshift = sql.Redshift(
         user=env_var.redshift_user,
         password=env_var.redshift_pw,
@@ -35,6 +39,11 @@ def main():
         port=env_var.redshift_port
     )
 
+    # On both QA and PROD, this will be connected to the PROD reporting Redshift database
+    # QA redshift does not have the required data in the cms_linear_svhedule_master and
+    # video_all_data so I am querying directly from the PROD reporting redshift and
+    # inserting into the QA reporting redshift for easier testing on Eric side when
+    # comparing data
     redshift1 = sql.Redshift(
         user=env_var.redshift_user,
         password=env_var.redshift_pw1,
@@ -45,14 +54,7 @@ def main():
 
     print("Getting last timestamp...")
 
-    redshift.execute(
-        """
-            SELECT next_job_start_time
-            FROM reporting_cron_job_logs
-            WHERE job_name='fastly_log_with_video_and_schedule_metadata' and status='DONE'
-            ORDER BY cron_end_time DESC;
-        """
-    )
+    redshift.execute(queries.getLastCompletedTime())
 
     # This is the query start time (i.e. this is 2020-10-28T00:00:00Z)
     # completed = time.strftime(
@@ -74,11 +76,8 @@ def main():
 
             print("Creating log entry...")
 
-            redshift.execute(
-                """
-                    INSERT INTO reporting_cron_job_logs (hashed_id, cron_start_time, job_name, status) VALUES ('{hashed_id}','{startStr}','fastly_log_with_video_and_schedule_metadata','RUNNING')
-                """.format(hashed_id=hashed_id, startStr=startStr)
-            )
+            redshift.execute(queries.initiateLog(
+                hashed_id=hashed_id, startStr=startStr))
 
             print("Created log entry")
             print("************************************************************")
@@ -90,88 +89,8 @@ def main():
             print("Query one hour later: " + oneHourLater)
 
             redshift1.execute(
-                """
-                SELECT
-                schedule.channel_id as id,
-                CASE WHEN linear.channel_name IS NULL THEN 'Unknown' ELSE linear.channel_name END as channel_name,
-                schedule.program_start_time as program_start_time,
-                schedule.program_end_time as program_end_time,
-                schedule.program_title as program_title,
-                schedule.video_title as video_title,
-                schedule.video_description as video_description,
-                schedule.video_start_time as video_start_time,
-                schedule.video_end_time as video_end_time,
-                ROW_NUMBER() OVER (PARTITION BY external_id, program_start_time, id
-                                                ORDER BY video_start_time desc) AS video_ranked,
-                schedule.external_id as external_id,
-                schedule.frequency_id as frequency_id,
-                upper(split_part(split_part(logs.distributor,'-',2), '/', 1)) as distributor,
-                sum(logs.minutes_watched) as minutes_watched
-                FROM (
-                SELECT timestamps, channel_id, distributor, minutes_watched
-                FROM fastly_log_aggregated_metadata
-                WHERE timestamps>='{time1}' and timestamps<'{time2}'
-                ) as logs
-                LEFT JOIN (
-                SELECT
-                    program.linear_channel_id as channel_id,
-                    program.schedule_start_time as program_start_time,
-                    program.schedule_end_time as program_end_time,
-                    video.linear_program_title as program_title,
-                    v.video_title as video_title,
-                    v.video_description as video_description,
-                    video.start_time as video_start_time,
-                    video.end_time as video_end_time,
-                    v.video_source_id as external_id,
-                    video.instruction_reference_asset_id as frequency_id
-                FROM (
-                    SELECT linear_channel_id, schedule_start_time, schedule_end_time, linear_program_id
-                    FROM
-                    (SELECT distinct linear_channel_id, schedule_start_time, schedule_end_time, linear_program_id,
-                            ROW_NUMBER() OVER (PARTITION BY linear_channel_id, schedule_start_time, schedule_end_time
-                                                ORDER BY schedule_update_date desc) AS ranked_num
-                    FROM cms_linear_schedule_master
-                    WHERE schedule_start_time>='{time1}' and schedule_start_time<'{time2}' and schedule_status<>'REMOVED'
-                    ORDER BY schedule_start_time) AS ranked
-                    WHERE ranked.ranked_num = 1
-                ) as program
-                LEFT JOIN (
-                    SELECT
-                    linear_program_title,
-                    linear_program_description,
-                    instruction_start_time,
-                    CASE WHEN DATE_PART(min, instruction_start_time) >= 30 THEN DATE_TRUNC('minutes', DATEADD(min, 1, instruction_start_time)) ELSE DATE_TRUNC('minutes', instruction_start_time) END as start_time,
-                    CASE WHEN DATE_PART(min, DATEADD('ms', instruction_duration_ms, instruction_start_time)) >= 30 THEN DATE_TRUNC('minutes', DATEADD(min, 1, DATEADD('ms', instruction_duration_ms, instruction_start_time))) ELSE DATE_TRUNC('minutes', DATEADD('ms', instruction_duration_ms, instruction_start_time)) END as end_time,
-                    instruction_reference_asset_id,
-                    linear_program_id
-                    FROM (
-                    SELECT linear_program_title, linear_channel_id, linear_program_description, instruction_start_time, instruction_reference_asset_id, instruction_duration_ms, linear_program_id, instruction_reference_start_ms,
-                        ROW_NUMBER() OVER (PARTITION BY linear_channel_id, linear_program_title, instruction_reference_asset_id, instruction_start_time
-                                        ORDER BY event_time desc) AS ranked_num
-                    FROM cms_linear_schedule_master
-                    WHERE instruction_type='VIDEO' OR instruction_type='INTERSTITIAL' and event_type='CREATE' and instruction_start_time>='{time3}' and instruction_start_time<'{time4}'
-                    ORDER BY instruction_start_time) AS ranked
-                    WHERE ranked.ranked_num = 1 and start_time>='{time1}' and start_time<'{time2}'
-                ) as video
-                ON video.linear_program_id=program.linear_program_id and video.start_time>=program.schedule_start_time and video.end_time<=program.schedule_end_time
-                LEFT JOIN video_all_data v
-                ON video.instruction_reference_asset_id=v.video_id
-                ) as schedule
-                ON logs.timestamps >= schedule.video_start_time and logs.timestamps < schedule.video_end_time and logs.channel_id=schedule.channel_id
-                LEFT JOIN (
-                    WITH ld AS (
-                    SELECT linear_channel_id, max("last_modified_date") AS latest
-                    FROM cms_linear_channel
-                    GROUP BY linear_channel_id
-                )
-                SELECT linear.linear_channel_id, linear.title as channel_name
-                FROM cms_linear_channel AS linear
-                JOIN ld ON ld.linear_channel_id = linear.linear_channel_id
-                WHERE linear."last_modified_date" = ld.latest
-                ) as linear
-                on linear.linear_channel_id=schedule.channel_id
-                GROUP BY id, channel_name, program_start_time, program_end_time, program_title, video_title, video_description, video_start_time, video_end_time, external_id, frequency_id, distributor
-                """.format(time1=completed, time2=newCompleted, time3=oneHourPrior, time4=oneHourLater)
+                query.fastlyLogWithVideoAndScheduleQuery(
+                    completed=completed, newCompleted=newCompleted, oneHourPrior=oneHourPrior, oneHourLater=oneHourLater)
             )
 
             args_str = b','.join(redshift.cursor.mogrify("(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", x)
@@ -195,11 +114,8 @@ def main():
             completedStr = time.strftime("%Y-%m-%dT%H:00:00Z", start)
 
             redshift.execute(
-                """
-                    UPDATE reporting_cron_job_logs
-                    SET status='DONE', cron_end_time='{endStr}', next_job_start_time='{completedStr}'
-                    WHERE job_name='fastly_log_with_video_and_schedule_metadata' AND hashed_id='{hashed_id}'
-                """.format(hashed_id=hashed_id, endStr=endStr, completedStr=completedStr)
+                queries.completedLog(hashed_id=hashed_id,
+                                     endStr=endStr, completedStr=completedStr)
             )
 
             print("Log updated...closing connection")
@@ -212,13 +128,8 @@ def main():
             print("Connection closed")
             sys.exit(-1)
     except:
-        redshift.execute(
-            """
-                UPDATE reporting_cron_job_logs
-                SET status='ERROR' and error_note='error'
-                WHERE job_name='fastly_log_with_video_and_schedule_metadata' AND hashed_id='{hashed_id}'
-            """.format(hashed_id=hashed_id, error=sys.exc_info()[0])
-        )
+        redshift.execute(queries.errorLog(
+            hashed_id=hashed_id, error=sys.exc_info()[0]))
 
         redshift.closeEverything()
 
