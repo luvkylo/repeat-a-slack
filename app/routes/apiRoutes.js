5b7eeafa66e0a42fbc468e168bf48b7c36ab256a
Alvin Ng <alvinhewittalvin@gmail.com>
2021-12-21 01:47:28 -0800

    first commit

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -124,6 +124,7 @@ module.exports = function (app) {
                             }
                         })
                             .then(resp => {
+                                console.log(resp.body)
                                 if (resp.body.message) {
                                     let txt = resp.body.message.text;
                                     let name = txt.match(/name:\s+(?<name>.+)/);
