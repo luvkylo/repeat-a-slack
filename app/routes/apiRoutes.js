9dd3ef48ce8e3e1ffa9980acef9f93b786d29ef6
Alvin Ng <alvinhewittalvin@gmail.com>
2021-12-20 22:33:28 -0800

    first commit

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const axios = require('axios');
 const { WebClient, ErrorCode } = require('@slack/web-api');
 
+let InAlarm = [];
+
 function sendMessage(client, msg) {
     try {
         setTimeout(() => {
@@ -16,6 +18,14 @@ function sendMessage(client, msg) {
         console.log(error.data);
     }
 }
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
 
 module.exports = function (app) {
     app.post("/api/slack", function (req, res) {
@@ -35,45 +45,56 @@ module.exports = function (app) {
                     }
                 })
                     .then(response => {
-                        if (response.data.plain_text && response.data.plain_text.match(/ALARM:/)) {
-                            let original_text = response.data.plain_text;
-                            let original_link = response.data.url_private;
-                            let found = '';
-                            let name = '';
-                            let time = '';
+                        if (response.data.plain_text) {
+                            if (response.data.plain_text.match(/ALARM:/)) {
+                                let original_text = response.data.plain_text;
+                                let original_link = response.data.url_private;
+                                let found = '';
+                                let name = '';
+                                let time = '';
 
-                            if (original_text.match(/\"Linear-\d+/)) {
-                                found = original_text.match(/\"Linear-(?<channel_id>\d+)/g);
-                                console.log(found.groups.channel_id);
-                                found = found.groups.channel_id
-                            }
-                            if (original_text.match(/Name:\s+.+/)) {
-                                name = original_text.match(/Name:\s+(?<name>.+)/);
+                                if (original_text.match(/\"Linear-\d+/)) {
+                                    found = original_text.match(/\"Linear-(?<channel_id>\d+)/g);
+                                    console.log(found.groups.channel_id);
+                                    found = found.groups.channel_id
+                                }
+                                if (original_text.match(/Name:\s+.+/)) {
+                                    name = original_text.match(/Name:\s+(?<name>.+)/);
+                                    console.log(name.groups.name);
+                                    name = name.groups.name;
+                                }
+                                if (original_text.match(/Timestamp:\s+.+/)) {
+                                    time = original_text.match(/Timestamp:\s+(?<timestamp>.+)/);
+                                    console.log(time.groups.timestamp);
+                                    time = time.groups.timestamp
+                                }
+
+                                InAlarm.push(name);
+
+                                let msg = 
+                                `
+                                ALARM from Repeat An Alert bot for Channel ${found}
+                                name: ${name}
+                                time: ${time}
+                                link to original issue: ${original_link}
+                                `;
+
+                                console.log('Starting message');
+
+                                sendMessage(web, msg);
+                            } else if (response.data.plain_text.match(/OK:/)) {
+                                let name = original_text.match(/Name:\s+(?<name>.+)/);
                                 console.log(name.groups.name);
                                 name = name.groups.name;
+                                if (InAlarm.includes(name)) {
+                                    console.log('Returning to normal');
+                                    removeItemOnce(InAlarm, name);
+                                }
                             }
-                            if (original_text.match(/Timestamp:\s+.+/)) {
-                                time = original_text.match(/Timestamp:\s+(?<timestamp>.+)/);
-                                console.log(time.groups.timestamp);
-                                time = time.groups.timestamp
-                            }
-
-                            let msg = 
-                            `
-                            ALARM from Repeat An Alert bot for Channel ${found}
-                            name: ${name}
-                            time: ${time}
-                            link to original issue: ${original_link}
-                            `;
-
-                            console.log('Starting message');
-
-                            sendMessage(web, msg);
                             // console.log(response.data.plain_text);
                             res.json({});
                         } else {
-
-                            console.log('Returning to normal');
+                            console.log('Unrecognized response...');
                             res.json({});
                         }
                     })
@@ -83,8 +104,13 @@ module.exports = function (app) {
             } else {
                 if (req.body.text && req.body.text.includes('Repeat An Alert')) {
                     let msg = req.body.text;
-                    sendMessage(web, msg);
-                    console.log('Repeating an alert...');
+                    let name = msg.match(/name:\s+(?<name>.+)/);
+                    console.log(name.groups.name);
+                    name = name.groups.name;
+                    if (InAlarm.includes(name)) {
+                        sendMessage(web, msg);
+                        console.log('Repeating an alert...');
+                    }
                     res.json({});
                 } else {
                     console.log(req.body);
