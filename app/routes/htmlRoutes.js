ce8b0a6b43555806bcb3c29ef3a86859bc106217
Alvin Ng <alvinhewittalvin@gmail.com>
2021-12-20 16:10:42 -0800

    first commit

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
