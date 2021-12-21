baa5bf9e77f4e4b0a385246e6db1a04ec6cc55dc
Alvin Ng <alvinhewittalvin@gmail.com>
2021-12-20 16:37:31 -0800

    first commit

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,8 +1,35 @@
+require('dotenv').config();
+
+const { WebClient, ErrorCode } = require('@slack/web-api');
+
+function sendMessage(client, msg) {
+    try {
+        setTimeout(() => {
+            client.chat.postMessage({
+                text: msg,
+                channel: '#alerts-playout',
+            });
+        }, 300000);
+    } catch (error) {
+        console.log(error.code);
+        console.log(error.data);
+    }
+}
+
 module.exports = function (app) {
     app.post("/api/slack", function (req, res) {
-        let challenge = req.body.challenge;
 
-        res.json({"challenge":challenge});
+        const token = process.env.SLACK_TOKEN;
+        const web = new WebClient(token);
+
+        sendMessage(web, 'test');
+
+        if (req.body.challenge) {
+            let challenge = req.body.challenge;
+            res.json({"challenge":challenge});
+        } else {
+            console.log(req);
+        }
     });
 
 };
