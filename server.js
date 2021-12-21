a5b3f3635d489bd711e8913fb3620df22f0c8a55
Alvin Ng <alvinhewittalvin@gmail.com>
2021-12-20 16:16:31 -0800

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
